dddcc139fc7ca05d6d41a5fb828aba44de4d20ff
Stewar <stewar1020@gmail.com>
2021-05-14 12:58:39 -0500

    Initial commit

---
+++ src/main/java/tewar/demo/ReactivoFinalApplication.java
@@ -0,0 +1,13 @@
+package tewar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivoFinalApplication.class, args);
+	}
+
+}
